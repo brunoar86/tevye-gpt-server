f3b6752fec1af7684404a156013f93e029fde2c0
Bruno Alberto Ribeiro <ribeiro256@msn.com>
2025-09-28 14:16:58 -0300

    feat(JWT): release - login, refresh and logout functions

--- tevye_gpt_server/src/main.py
+++ tevye_gpt_server/src/main.py
@@ -5,7 +5,7 @@ from tevye_gpt_server.src.routes import conversation
 from tevye_gpt_server.src.routes import auth
 
 app = FastAPI(title='Tevye GPT Server', docs_url='/swagger',
-              openapi_url='/openapi.json', version='0.2.0')
+              openapi_url='/openapi.json', version='0.3.0')
 
 app.include_router(health.router)
 app.include_router(conversation.router)
